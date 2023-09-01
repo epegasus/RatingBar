9785305f59d69d66d2e014ee552553e1ca9bee97
Sohaib Ahmed <sohaibahmed.hypersoft@gmail.com>
2023-09-01 12:37:45 +0500

    RatingBar

--- ratingBar/build.gradle.kts
+++ ratingBar/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     }
 
     buildTypes {
-        release {
+        debug {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
